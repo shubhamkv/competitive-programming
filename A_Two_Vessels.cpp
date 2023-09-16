d1b7af199302f0625c4c8f961cf1336db34de2ef
Shubham Kumar <87669020+shubhamkv@users.noreply.github.com>
2023-09-16 14:06:44 +0530

    initial commit

---
+++ A_Two_Vessels.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define pb push_back;
+#define mkp make_pair;
+#define all(v) v.begin(), v.end()
+const int M = 1e9 + 7;
+const int N = 1e5 + 5;
+using namespace std;
+void solve()
+{
+   ll n,a,b,c,m,k,p,q,r,x,y,z,cnt=0;
+   double d1,d2,d3,d4;
+   string s1,s2;
+   vector<int> v1,v2;
+   map<int,int> mp;
+   set<int> s;
+              
+   cin>>a>>b>>c;
+   vector<int> arr();
+                     
+   //   Agar sale bina edge cases check kiye hua submit kiya toh gaand marrao !!!
+                     
+   double maxi = max(a,b);
+   double mini = min(a,b);
+   
+  while(maxi>mini)
+  {
+     double diff = (maxi-mini);
+     if(diff>c)
+     {
+        maxi-=c;
+        mini+=c;
+     }
+     else
+     {
+        double diff2 = (maxi-mini)/2;
+        maxi-=diff2;
+        mini+=diff2;
+     }
+     cnt++;
+  }
+  cout<<cnt<<endl;
+}
+int main()
+{
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+   ll t,a,b,c;
+   cin>>t;
+   while(t--)
+   {
+      solve();
+   }
+   return 0;
+}
