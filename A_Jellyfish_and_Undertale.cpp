1f4afbac47179fe5deb8a97550605b565e2ada9f
Shubham Kumar <87669020+shubhamkv@users.noreply.github.com>
2024-01-01 23:17:13 +0530

    initial commit

---
+++ A_Jellyfish_and_Undertale.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define pb push_back;
+#define mkp make_pair;
+#define all(v) v.begin(), v.end()
+const int M = 1e9 + 7;
+const int N = 1e5 + 5;
+using namespace std;
+void solve()
+{
+   ll n,a,b,c,m,k,p,q,r,x,y,z,sum=0;
+   double d1,d2,d3,d4;
+   string s1,s2;
+   vector<int> v1,v2;
+   map<int,int> mp;
+   set<int> s;
+              
+   cin>>a>>b>>n;
+   vector<ll> arr(n);
+   for(int i=0;i<n;i++)
+    cin>>arr[i];
+    sum=b;
+   //   Agar sale bina edge cases check kiye hua submit kiya toh gaand marrao !!!
+                     
+   for(int i=0;i<n;i++)
+   {
+      sum+=min(arr[i],a-1);
+   }
+   cout<<sum<<endl;
+}
+int main()
+{
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+   ll t,a,b,c;
+   cin>>t;
+   while(t--)
+   {
+        solve();
+   }
+   return 0;
+}
