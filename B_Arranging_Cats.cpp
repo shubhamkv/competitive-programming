124c37da7ead0bd954e6ff55f34aa5e078a08499
Shubham Kumar <87669020+shubhamkv@users.noreply.github.com>
2024-01-17 20:05:01 +0530

    initial commit

---
+++ B_Arranging_Cats.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define pb push_back;
+#define mkp make_pair;
+#define all(v) v.begin(), v.end()
+const int M = 1e9 + 7;
+const int N = 1e5 + 5;
+using namespace std;
+void solve()
+{
+   ll n,a,b,c,m,k,p,q,r,x,y,z;
+   double d1,d2,d3,d4;
+   string s1,s2;
+   vector<int> v1,v2;
+   map<int,int> mp;
+   set<int> s;
+              
+   cin>>n;
+   //vector<int> arr(n);
+  // for(int i=0;i<n;i++)
+  //   cin>>arr[i];
+
+   //   Agar sale bina edge cases check kiye hua submit kiya toh gaand marrao !!!
+                     
+   cin>>s1;
+   cin>>s2;
+   int zero_one=0, one_zero=0;
+   for(int i=0;i<n;i++)
+   {
+      if(s1[i]=='0' && s2[i]=='1')
+        zero_one++;
+      else if(s1[i]=='1' && s2[i]=='0')
+        one_zero++;
+   }
+   cout<<max(zero_one,one_zero)<<endl;
+}
+int main()
+{
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+   ll t,a,b,c;
+   cin>>t;
+   while(t--)
+   {
+        solve();
+   }
+   return 0;
+}
